89736da7b2eeae151688f82b542b323868e0037f
Ivan <matveev92iv@icloud.com>
2021-12-08 18:13:50 +0300

    Initial commit

---
+++ src/com/metanit/testauthor.java
@@ -0,0 +1,11 @@
+package com.metanit;
+public class testauthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Miki Liv", "kjhb@mail.com", 'M');
+        Author a2 = new Author("Nora Ukiti", "jbnhg@mail,com", 'U');
+        Author a3 = new Author("Laura Novil", "njhfrd@mail.com", 'F');
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
